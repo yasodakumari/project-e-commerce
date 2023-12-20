b5863a7fb94c9161f032df1363552192c1a87972
yasodakumari <136498356+yasodakumari@users.noreply.github.com>
2023-12-20 22:59:39 +0530

    Add files via upload

---
+++ Sidebar.js
@@ -0,0 +1,26 @@
+import "./sidebar.css";
+import Category from "./Category/Category";
+import Price from "./Price/Price";
+import Colors from "./Colors/Colors";
+
+
+ function Sidebar({handleChange}) {
+
+  return(
+   <>
+  <section className="sidebar">
+    <div className="logo-container">
+    <h1>cart</h1>
+    </div>
+   
+   <Category handleChange={handleChange}/>
+   <Price handleChange={handleChange}/>
+   <Colors handleChange={handleChange}/>
+
+  </section>
+  </>
+  );
+}
+
+
+export default Sidebar;
